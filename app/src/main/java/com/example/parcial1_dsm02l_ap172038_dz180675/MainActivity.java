b0563f3353582b1b394c5286ff707c9e0f956b81
JosueAvelar <avelarjosue1@gmail.com>
2022-03-05 15:48:17 -0600

    Primer commit del Parcial c:

---
+++ app/src/main/java/com/example/parcial1_dsm02l_ap172038_dz180675/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.parcial1_dsm02l_ap172038_dz180675;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
